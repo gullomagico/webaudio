3b251c82309941ee17d69608d9c7724515368ba5
GulloMagico <33353590+gullomagico@users.noreply.github.com>
2023-07-23 14:47:57 +0200

    Fix routing config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,8 @@ export default defineConfig({
   integrations: [tailwind()],
   site: 'https://gullomagico.github.io',
   base: '/webaudio',
+  trailingSlash: 'never',
+  build: {
+    format: 'file'
+  }
 });
