e8cace84d4ba2b45472781468456f021773fe20e
GulloMagico <33353590+gullomagico@users.noreply.github.com>
2023-07-23 04:49:34 +0200

    Fix imports with base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://gullomagico.github.io/webaudio',
-  base: '/',
+  site: 'https://gullomagico.github.io',
+  base: '/webaudio',
 });
