d157bd33f7bf6d13507f37b99727029f102cb720
GulloMagico <33353590+gullomagico@users.noreply.github.com>
2023-08-13 13:49:45 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  site: 'https://gullomagico.github.io',
+  integrations: [tailwind(), react(), sitemap()],
+  site: 'https://cafa.dev',
   base: '/webaudio',
   trailingSlash: 'never',
   build: {
-    format: 'file',
-  },
+    format: 'file'
+  }
 });
